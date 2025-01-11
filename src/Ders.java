a097a4c5be607d9642291c0bff5d80443b716282
Murad MacBook pro <xakus22@gmail.com>
2025-01-11 19:25:05 +0400

    Create new app

---
+++ src/Ders.java
@@ -0,0 +1,12 @@
+public class Ders {
+    public static void main(String[] args){
+        System.out.println("_|       Hello World        |");
+        System.out.println(" |--------------------------|");
+        for(int i=0;i<100;i++){
+                System.out.println(i+"|  i="+i+"                |");
+        }
+        System.out.println("-|--------------------------|");
+
+    }
+}
+
